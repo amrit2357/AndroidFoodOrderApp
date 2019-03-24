03b2335b68b25fed4e8001f707973371c1906fe7
amrit2357 <32218383+amrit2357@users.noreply.github.com>
2019-03-25 03:48:48 +0530

    Update Common.java
    convertCodeToStatus function Added to common

--- app/src/main/java/com/example/orderfoodnow/Common/Common.java
+++ app/src/main/java/com/example/orderfoodnow/Common/Common.java
@@ -4,6 +4,17 @@ import com.example.orderfoodnow.Model.User;
 
 public class Common {
     public static User currentUser;
-
+    
+    public static String convertCodeToStatus(String code){
+        if(code.equals( "0" )){
+            return "Placed";
+        }else if(code.equals( "1" )){
+            return "on my way";
+        }else if(code.equals( "2" )){
+            return "Shipped";
+        }else{
+            return "Delivered";
+        }
+    }
 
 }
